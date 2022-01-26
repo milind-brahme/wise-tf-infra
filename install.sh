6c99d2edde4817f2cdf1ee5a37e38de0c8dbcc05
Milind Brahme <milindbrahme@Milinds-MacBook-Air.local>
2022-01-25 22:49:19 -0500

    25jan2022

--- install.sh
+++ install.sh
@@ -7,4 +7,13 @@ sudo yum install /home/ec2-user/efs/milind/sqlclientBinaries/oracle-instantclien
 sudo yum install /home/ec2-user/efs/milind/sqlclientBinaries/oracle-instantclient-sqlplus-21.4.0.0.0-1.el8.x86_64.rpm -y
 sudo amazon-linux-extras enable nginx1
 sudo yum install nginx -y
+sudo yum install nginx-mod-stream.x86_64 -y
+sudo sh -c "echo 'stream {
 
+    server {
+        listen     9090;
+        #TCP traffic will be forwarded to the specified server
+        proxy_pass 127.0.0.1:12346;
+    }
+}' >> /etc/nginx/nginx.conf "
+sudo service nginx restart
--- main.tf
+++ main.tf
@@ -82,13 +82,13 @@ resource "aws_security_group" "defaultSecurityGrp" {
         "0.0.0.0/0"
       ],
       "description" : "",
-      "from_port" : 9001,
+      "from_port" : 9090,
       "ipv6_cidr_blocks" : [],
       "prefix_list_ids" : [],
       "protocol" : "tcp",
       "security_groups" : [],
       "self" : false,
-      "to_port" : 9001
+      "to_port" : 9090
     },
     {
       cidr_blocks = [
